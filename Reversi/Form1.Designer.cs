cc001be03ebc3bf9723cdfee6c342c8846f866c6
Qweque <t.q.vanderspijk@students.uu.nl>
2017-10-25 13:22:09 +0200

    Initial commit

---
+++ Reversi/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace Reversi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        
+        public void InitializeComponent()
+        {
+            this.Reset = new System.Windows.Forms.Button();
+            this.Help = new System.Windows.Forms.Button();
+            this.speelveld = new System.Windows.Forms.Panel();
+            this.keer = new System.Windows.Forms.Label();
+            this.x_veld = new System.Windows.Forms.TextBox();
+            this.y_veld = new System.Windows.Forms.TextBox();
+            this.max = new System.Windows.Forms.Label();
+            this.Error = new System.Windows.Forms.Label();
+            this.Playercounter = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Reset
+            // 
+            this.Reset.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Reset.Location = new System.Drawing.Point(50, 30);
+            this.Reset.Name = "Reset";
+            this.Reset.Size = new System.Drawing.Size(150, 30);
+            this.Reset.TabIndex = 0;
+            this.Reset.Text = "Start spel";
+            this.Reset.UseVisualStyleBackColor = true;
+            this.Reset.Click += new System.EventHandler(this.Nieuw_Spel);
+            // 
+            // Help
+            // 
+            this.Help.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Help.Location = new System.Drawing.Point(550, 30);
+            this.Help.Name = "Help";
+            this.Help.Size = new System.Drawing.Size(150, 30);
+            this.Help.TabIndex = 1;
+            this.Help.Text = "Help";
+            this.Help.UseVisualStyleBackColor = true;
+            // 
+            // speelveld
+            // 
+            this.speelveld.Location = new System.Drawing.Point(20, 100);
+            this.speelveld.Name = "speelveld";
+            this.speelveld.Size = new System.Drawing.Size(5000, 5000);
+            this.speelveld.TabIndex = 2;
+            this.speelveld.Paint += new System.Windows.Forms.PaintEventHandler(this.Tekenspeelveld);
+            this.speelveld.MouseClick += new System.Windows.Forms.MouseEventHandler(this.speelveld_MouseClick);
+            // 
+            // keer
+            // 
+            this.keer.AutoSize = true;
+            this.keer.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.keer.Location = new System.Drawing.Point(425, 35);
+            this.keer.Name = "keer";
+            this.keer.Size = new System.Drawing.Size(17, 20);
+            this.keer.TabIndex = 3;
+            this.keer.Text = "x";
+            // 
+            // x_veld
+            // 
+            this.x_veld.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.x_veld.Location = new System.Drawing.Point(350, 32);
+            this.x_veld.Name = "x_veld";
+            this.x_veld.Size = new System.Drawing.Size(50, 27);
+            this.x_veld.TabIndex = 4;
+            // 
+            // y_veld
+            // 
+            this.y_veld.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.y_veld.Location = new System.Drawing.Point(470, 32);
+            this.y_veld.Name = "y_veld";
+            this.y_veld.Size = new System.Drawing.Size(50, 27);
+            this.y_veld.TabIndex = 5;
+            // 
+            // max
+            // 
+            this.max.AutoSize = true;
+            this.max.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.max.Location = new System.Drawing.Point(230, 35);
+            this.max.Name = "max";
+            this.max.Size = new System.Drawing.Size(95, 20);
+            this.max.TabIndex = 6;
+            this.max.Text = "max: 15 x 8";
+            // 
+            // Error
+            // 
+            this.Error.AutoSize = true;
+            this.Error.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Error.Location = new System.Drawing.Point(346, 68);
+            this.Error.Name = "Error";
+            this.Error.Size = new System.Drawing.Size(0, 20);
+            this.Error.TabIndex = 7;
+            // 
+            // Playercounter
+            // 
+            this.Playercounter.AutoSize = true;
+            this.Playercounter.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Playercounter.Location = new System.Drawing.Point(747, 38);
+            this.Playercounter.Name = "Playercounter";
+            this.Playercounter.Size = new System.Drawing.Size(0, 20);
+            this.Playercounter.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1040, 610);
+            this.Controls.Add(this.Playercounter);
+            this.Controls.Add(this.Error);
+            this.Controls.Add(this.max);
+            this.Controls.Add(this.y_veld);
+            this.Controls.Add(this.x_veld);
+            this.Controls.Add(this.keer);
+            this.Controls.Add(this.speelveld);
+            this.Controls.Add(this.Help);
+            this.Controls.Add(this.Reset);
+            this.Name = "Form1";
+            this.Text = "Reversi";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+            #endregion
+
+
+        private System.Windows.Forms.Button Reset;
+        private System.Windows.Forms.Button Help;
+        private System.Windows.Forms.Panel speelveld;
+        private System.Windows.Forms.Label keer;
+        private System.Windows.Forms.TextBox x_veld;
+        private System.Windows.Forms.TextBox y_veld;
+        private System.Windows.Forms.Label max;
+        private System.Windows.Forms.Label Error;
+        private System.Windows.Forms.Label Playercounter;
+    }
+
+}
+
---
+++ Reversi/Form1.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Reversi
+{
+    public partial class Form1 : Form
+    {
+        
+        int veldx = 6; int veldy = 6;        
+        int playercounter = 1;
+        bool plaatcheck;
+        int[,] bol = new int[15, 8];
+        int[,] help = new int[15, 8];
+        const int positie = 50;
+        const int BolSize = positie - 2;
+        SolidBrush rood = new SolidBrush(Color.FromArgb(255, 0, 0));
+        SolidBrush blauw = new SolidBrush(Color.FromArgb(0, 0, 255));
+
+        public Form1()
+        {
+            InitializeComponent();            
+        }
+
+                        
+        private void Tekenspeelveld(object sender, PaintEventArgs pea)
+        {
+            
+            Graphics gr = pea.Graphics;
+            gr.DrawRectangle(Pens.Black, 0, 0, veldx * positie, veldy * positie);
+            y_veld.Text = veldy.ToString();
+            x_veld.Text = veldx.ToString();
+
+            //Playercounter
+            if (playercounter == 2)
+            {
+                Playercounter.Text = "Rood is aan zet";
+                Playercounter.ForeColor = Color.Red;
+            }
+            else
+            {
+                Playercounter.Text = "Blauw is aan zet";
+                Playercounter.ForeColor = Color.Blue;
+            }
+
+            //Veld
+            for (int x = 1; x < veldx; x++)
+            {
+                gr.DrawLine(Pens.Black, positie * x, 0, positie * x, veldy * positie);                                
+            }
+                        
+
+            for (int y = 1; y < veldy; y++)
+            {
+                gr.DrawLine(Pens.Black, 0, positie * y, veldx * positie, positie * y);
+            }
+
+            
+
+            //Teken de steentjes
+            for (int n = 0; n < veldx; n++)
+                for (int m = 0; m < veldy; m++)
+                {
+                    if (bol[n,m] == 1)
+                    {
+                        this.TekenBol(pea.Graphics, rood, positie * n + 1, positie * m + 1);
+                    }
+                    if (bol[n,m] == 2)
+                    {
+                        this.TekenBol(pea.Graphics, blauw, positie * n + 1, positie * m + 1);
+                    }
+                }
+
+        }
+
+        public void Nieuw_Spel(object sender, EventArgs ea)
+        {
+            //Haal de array leeg
+            for (int n = 0; n < veldx; n++)
+                for (int m = 0; m < veldy; m++)
+                {
+                    bol[n, m] = 0;
+                }
+            try
+            {
+                veldx = Int32.Parse(x_veld.Text);
+                    if (veldx > 15)
+                        veldx = 15;
+                    if (veldx < 3)
+                        veldx = 3;
+                veldy = Int32.Parse(y_veld.Text);
+                    if (veldy > 8)
+                        veldy = 8;
+                    if (veldy < 3)
+                        veldy = 3;
+                this.Error.Text = "";
+                this.Reset.Text = "Nieuw spel";
+                speelveld.Invalidate();
+            }
+            catch
+            {
+                Error.Text = "vul een geldige waarde in";
+            }
+
+            //Vul de array met de beginsituatie
+            for (int n = 0; n < veldx; n++)
+                for (int m = 0; m < veldy; m++)
+                {
+                    if (n == veldx / 2 && m == veldy / 2)
+                    {
+                        bol[n, m] = 1;
+                        bol[n - 1, m - 1] = 1;
+                        bol[n - 1, m] = 2;
+                        bol[n, m - 1] = 2;
+                    }
+                }
+            
+            playercounter = 1;
+        }
+
+        private void speelveld_MouseClick(object sender, MouseEventArgs mea)
+        {
+            //Playercounter switcher
+            if (playercounter == 1)
+                playercounter++;
+            else
+                playercounter--;
+
+            //Vul de array met de juiste waarde op de juiste plek
+            int vakjex = mea.X / positie;
+            int vakjey = mea.Y / positie;
+                        
+            Plaatscheck(playercounter, mea.X, mea.Y);
+
+            if (playercounter == 1 && plaatcheck)
+                bol[vakjex, vakjey] = 1;
+            if (playercounter == 2 && plaatcheck)
+                bol[vakjex, vakjey] = 2;
+
+            
+
+            speelveld.Invalidate();
+        }
+
+
+        public void TekenBol(Graphics gr, Brush p, int x, int y)
+        {
+            gr.FillEllipse(p, x, y, BolSize, BolSize);
+        }
+
+        public void TekenHelp(Graphics gr, int x, int y)
+        {
+            gr.DrawEllipse(Pens.Black, x, y, BolSize / 2, BolSize / 2);
+        }
+
+        // maak een methode voor het checken van de omliggenden plaatsen 
+        public bool Plaatscheck(int playercounter, int x, int y)
+        {
+            plaatcheck = false;
+            int p = playercounter;
+
+            //boven
+            if (bol[x, y] == 0)
+                if (bol[x, y - 1] != p && p != 0)
+                    ZetCheck(p, x, y, 0, -1);
+
+            //rechtboven
+            if (bol[x, y] == 0)
+                if (bol[x + 1, y - 1] != p && p != 0)
+                    ZetCheck(p, x, y, 1, -1);
+
+            //rechts
+            if (bol[x, y] == 0)
+                if (bol[x + 1, y] != p && p != 0)
+                    ZetCheck(p, x, y, 1, 0);
+
+            //rechtsonder
+            if (bol[x, y] == 0)
+                if (bol[x + 1, y + 1] != p && p != 0)
+                    ZetCheck(p, x, y, 1, 1);
+
+            //onder
+            if (bol[x, y] == 0)
+                if (bol[x, y + 1] != p && p != 0)
+                    ZetCheck(p, x, y, 0, 1);
+
+            //linksonder
+            if (bol[x, y] == 0)
+                if (bol[x - 1, y + 1] != p && p != 0)
+                    ZetCheck(p, x, y, - 1, 1);
+
+            //links
+            if (bol[x, y] == 0)
+                if (bol[x - 1, y] != p && p != 0)
+                    ZetCheck(p, x, y, -1, 0);
+
+            //linksboven
+            if (bol[x, y] == 0)
+                if (bol[x - 1, y - 1] != p && p != 0)
+                    ZetCheck(p, x, y, -1, -1);
+
+            return plaatcheck;
+        
+        }
+        // maak een methode die checkt in de richting van de gevonden steen
+        public bool ZetCheck (int playercounter, int x, int y, int dx, int dy)
+        {
+            int mx;
+            int my;
+            int p = playercounter;
+
+            //rechts
+            if(dx > 0)
+            {
+                if (dy > 0)
+                {
+                    for (mx = x; mx + dx < 15; x++)
+                        for (my = y; my + dy < 15; x++)
+                        {
+                            x += dx;
+                            y += dy;
+
+                            if (bol[x, y] == p)
+                            {
+                                plaatcheck = true;
+                                return plaatcheck;
+                            }
+
+                        }
+                }
+
+                if(dy == 0)
+                {
+
+                }
+
+                if (dy < 0)
+                {
+
+                }
+            }
+
+            //boven/onder
+            if(dx == 0)
+                if (dy > 0)
+                {
+
+                }
+
+            if (dy == 0)
+            {
+
+            }
+
+            if (dy < 0)
+            {
+
+            }
+
+            //links
+            if (dx < 0)
+                if (dy > 0)
+                {
+
+                }
+
+            if (dy == 0)
+            {
+
+            }
+
+            if (dy < 0)
+            {
+
+            }
+
+            return plaatcheck;
+        }
+        
+        
+    }
+}
